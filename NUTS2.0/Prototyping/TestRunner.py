a0df538e8e1113b280448bf47c3851053a52093b
Janik <janik.schlatter@gmail.com>
2020-04-04 14:32:54 +0200

    Update TestRunner

--- NUTS2.0/Prototyping/TestRunner.py
+++ NUTS2.0/Prototyping/TestRunner.py
@@ -1,3 +1,8 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+
+
 class TestRunner:
     logger = 0
     connection = 0
@@ -5,5 +10,21 @@ class TestRunner:
     testResult = 0
 
     def run_all_tests(self):
-        for test in self.testSuite:
-            test.run()
+        commands = "show ip int brief"
+        nr = InitNornir(config_file="Configurations/config.yaml")
+        print(nr.inventory.hosts)
+        result = nr.run(
+            task=netmiko_send_command,
+            command_string=commands
+        )
+
+        print_result(result)
+
+
+def main():
+    runner = TestRunner()
+    runner.run_all_tests()
+
+
+if __name__ == '__main__':
+    main()
