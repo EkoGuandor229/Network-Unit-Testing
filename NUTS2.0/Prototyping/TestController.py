c83f8a68931505dfca458c7011d5353151073b36
Janik <janik.schlatter@gmail.com>
2020-04-07 12:27:57 +0200

    Update TestController

--- NUTS2.0/Prototyping/TestController.py
+++ NUTS2.0/Prototyping/TestController.py
@@ -1,15 +1,18 @@
+from Prototyping.Evaluator import Evaluator
 from Prototyping.TestBuilder import TestBuilder
+from Prototyping.TestRunner import TestRunner
 
 
 class TestController:
     reporter = 0
-    testRunner = 0
-    evaluator = 0
+    testRunner = TestRunner()
+    evaluator = Evaluator()
     testBuilder = TestBuilder()
 
-    def logic(self):
-        print("It just works " + str(self.reporter))
-
+    def __init__(self):
+        self.testBuilder.create_test_suite()
+        self.testRunner.run_all_tests()
+        self.evaluator.compare()
 
 def main():
     controller = TestController()
