f466774e01b344a352f95fbfe3fbf80e27422313
Janik <janik.schlatter@gmail.com>
2020-04-12 17:38:21 +0200

    Add Error Handling to FileHandler
    Fix Path Problem

--- NUTS2.0/nuts/utilities/file_handler.py
+++ NUTS2.0/nuts/utilities/file_handler.py
@@ -3,35 +3,48 @@ import yaml
 
 class FileHandler:
     logger = 0
-    devicePath = r"../nuts/resources/inventory/Devices/devices.yaml"
-    deviceConnectionPath = r"../nuts/resources/inventory/DeviceConnections/deviceconnections.yaml"
-    testDefinitionPath = r"../nuts/resources/inventory/TestDefinitions/testDefinitions.yaml"
-    fileType = 0
+    devicePath = r"../resources/inventory/Devices/devices.yaml"
+    deviceConnectionPath = r"../resources/inventory/DeviceConnections/deviceconnections.yaml"
+    testDefinitionPath = r"../resources/inventory/TestDefinitions/testDefinitions.yaml"
 
     def read_device_file(self):
-        with open(self.devicePath) as file:
-            devices_yaml = []
-            device_list = yaml.full_load(file)
-            for device, dev in device_list.items():
-                devices_yaml.append(dev)
-            return devices_yaml
+        try:
+            with open(self.devicePath) as file:
+                devices_yaml = []
+                device_list = yaml.full_load(file)
+                try:
+                    for device, dev in device_list.items():
+                        devices_yaml.append(dev)
+                    return devices_yaml
+                except ValueError:
+                    print("Device Information are not entered correctly")
+        except IOError:
+            print("Inputfile not found")
 
     def read_device_connection_file(self):
-        with open(self.deviceConnectionPath) as file:
-            device_connection_yaml = []
-            device_connection_list = yaml.full_load(file)
-            for device_connection, devcon in device_connection_list.items():
-                device_connection_yaml.append(devcon)
-            return device_connection_yaml
+        try:
+            with open(self.deviceConnectionPath) as file:
+                device_connection_yaml = []
+                device_connection_list = yaml.full_load(file)
+                try:
+                    for device_connection, devcon in device_connection_list.items():
+                        device_connection_yaml.append(devcon)
+                    return device_connection_yaml
+                except ValueError:
+                    print("DeviceConnection Information are not in the right Format")
+        except IOError:
+            print("Inputfile not found")
 
     def read_test_definition_file(self):
-        with open(self.testDefinitionPath) as file:
-            test_definition_yaml = []
-            test_definition_list = yaml.full_load(file)
-            for test_definition, tedef in test_definition_list.items():
-                test_definition_yaml.append(tedef)
-            return test_definition_yaml
-
-    def write_file(self, dict_file):
-        with open(self.path, 'w') as file:
-            devices = yaml.dump(dict_file, file)
+        try:
+            with open(self.testDefinitionPath) as file:
+                test_definition_yaml = []
+                test_definition_list = yaml.full_load(file)
+                try:
+                    for test_definition, tedef in test_definition_list.items():
+                        test_definition_yaml.append(tedef)
+                    return test_definition_yaml
+                except ValueError:
+                    print("TestDefinition Information are not in the right Format")
+        except IOError:
+            print("Inputfile not found")
