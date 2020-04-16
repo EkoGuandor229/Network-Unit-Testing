456535e0208daf07b8139d1a14390657a7199d69
Janik <janik.schlatter@gmail.com>
2020-04-16 11:23:39 +0200

    Update Inventorymanagement and TestdefinitionLoader

--- NUTS2.0/nuts/inventorymanagement/network_test_definition.py
+++ NUTS2.0/nuts/inventorymanagement/network_test_definition.py
@@ -1,28 +1,31 @@
 class TestDefinition:
     testId = 0
     command = 0
-    testDevices = []
+    testDevice = 0
     target = 0
     expectedResult = 0
 
-    def __init__(self, testId, command, testDevices, target, expectedResult):
+    def __init__(self, testId, command, test_device, target, expected_result):
         self.testId = testId
         self.command = command
-        self.testDevices = testDevices
+        self.testDevice = test_device
         self.target = target
-        self.expectedResult = expectedResult
+        self.expectedResult = expected_result
+
+    def print_test_definition(self):
+        print(self.testId, self.command, self.testDevice, self.target, self.expectedResult)
 
     def get_command(self):
         return self.command
 
-    def print_test_definition(self):
-        print(self.testId, self.command, self.testDevices, self.target, self.expectedResult)
-
     def get_test_devices(self):
-        return self.testDevices
-
-    def set_test_devices(self, test_devices):
-        self.testDevices = test_devices
+        return self.testDevice
 
     def get_target(self):
         return self.target
+
+    def get_expected_result(self):
+        return self.expectedResult
+
+    def set_test_device(self, test_device):
+        self.testDevice = test_device
